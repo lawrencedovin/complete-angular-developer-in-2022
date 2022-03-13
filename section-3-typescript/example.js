3e667fd329d53b3402fc462adacd175da697591e
Lawrence Dovin <lawrencedovin@gmail.com>
2022-03-13 18:03:48 -0400

    Added initial typescript files

---
+++ section-3-typescript/example.js
@@ -0,0 +1,5 @@
+function addShipping(price, shipping) {
+    var total = price + shipping;
+    console.log("$".concat(total));
+}
+addShipping(10, 2);
---
+++ section-3-typescript/example.ts
@@ -0,0 +1,6 @@
+function addShipping(price: number, shipping: number) {
+    let total = price + shipping;
+    console.log(`$${total}`);
+}
+
+addShipping(10, 2);
