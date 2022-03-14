3868fc1dd42e14c67ab3ebfc054df67100a78960
Lawrence Dovin <lawrencedovin@gmail.com>
2022-03-14 13:24:42 -0400

    Added input example

--- section-4-component-fundamentals/src/app/app.component.ts
+++ section-4-component-fundamentals/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'section-4-component-fundamentals';
+  imgURL = "https://images.unsplash.com/photo-1601979031925-424e53b6caaa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80";
+
+  changeImage(e: KeyboardEvent) {
+    this.imgURL = (e.target as HTMLInputElement).value;
+  }
 }
